e14b7a1868c362954f9d20e31f85b2604408c623
Pascal Piche <pascal@nitriques.com>
2016-07-27 21:35:29 -0400

    Added new BasicRalNode implementation

---
+++ Common.Public/RAL/BasicRalNode.cs
@@ -0,0 +1,12 @@
+﻿
+namespace OHM.RAL
+{
+    public class BasicRalNode : RalNodeAbstract
+    {
+        public BasicRalNode(string key, string name) : base(key, name) {}
+
+        protected override void RegisterCommands() {}
+
+        protected override void RegisterProperties() {}
+    }
+}
