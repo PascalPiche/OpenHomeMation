4ff50db862cabe206fbc697a6d77613861376d6c
Pascal Piche <pascal@nitriques.com>
2018-01-22 19:45:16 -0500

    More work on the windows service

--- OpenHomeMationService/OpenHomeMationService.cs
+++ OpenHomeMationService/OpenHomeMationService.cs
@@ -9,93 +9,138 @@ using System.ServiceProcess;
 
 namespace OpenHomeMationService
 {
+    /// <summary>
+    /// Core windows service class for Open Home Mation
+    /// </summary>
     public partial class OpenHomeMationService : ServiceBase
     {
-        private OpenHomeMation ohm;
+        #region Private member
 
+        /// <summary>
+        /// Main instance 
+        /// </summary>
+        private static OpenHomeMation ohm;
+
+        /// <summary>
+        /// Main log instance
+        /// </summary>
+        private static ILogger log;
+
+        /// <summary>
+        /// Configured log level from data configuration or args passed
+        /// </summary>
+        private static int logLevel = 0;
+
+        #endregion
+
+        #region Public Ctor
+
+        /// <summary>
+        /// Core constructor. 
+        /// Set all service properties listed in the remarks.
+        /// </summary>
+        /// <remarks>
+        /// AutoLog.....................: true
+        /// CanHandlePowerEvent.........: true
+        /// CanHandleSessionChangeEvent.: false
+        /// CanPauseAndContinue.........: false
+        /// CanShutdown.................: true
+        /// CanStop.....................: true
+        /// ServiceName.................: OvWra
+        /// </remarks>
         public OpenHomeMationService()
         {
+            //Set properties
             this.AutoLog = true;
             this.CanHandlePowerEvent = true;
             this.CanHandleSessionChangeEvent = false;
-            this.CanPauseAndContinue = true;
+            this.CanPauseAndContinue = false;
             this.CanShutdown = true;
             this.CanStop = true;
             this.ServiceName = "OvWra";
+
+            //Initialise component
             InitializeComponent();
         }
 
+        #endregion
+
+        #region Protected override
+
+        /// <summary>
+        /// Create all object needed for the system and start the system.
+        /// </summary>
+        /// <param name="args">Arguments configuring the system</param>
         protected override void OnStart(string[] args)
         {
+            ParseArgs(args);
 
-            ILoggerManager loggerMng;
-            IPluginsManager pluginMng;
-            IInterfacesManager interfacesMng;
-            IVrManager vrMng;
+            //TODO: We need to setup LoggerManager based on the args
+            ILoggerManager loggerMng = new LoggerManager();
 
-            loggerMng = new LoggerManager();
-            var dataMng = new FileDataManager(AppDomain.CurrentDomain.BaseDirectory + "\\data\\");
-            pluginMng = new PluginsManager(loggerMng, AppDomain.CurrentDomain.BaseDirectory + "\\plugins\\");
-            interfacesMng = new InterfacesManager(loggerMng, pluginMng);
-            vrMng = new VrManager(loggerMng, pluginMng);
+            //Get Internal logger
+            ILogger log = loggerMng.GetLogger("OHM", "Service");
+
+            //FROM HERE WE SHOULD USE INTERNAL LOG
+
+            //TODO: Extract Data directory from args
+            DataManagerAbstract dataMng = new FileDataManager(AppDomain.CurrentDomain.BaseDirectory + "\\data\\");
+
+            //TODO: Extract Plugin directory from data And/Or args
+            IPluginsManager pluginMng = new PluginsManager(loggerMng, AppDomain.CurrentDomain.BaseDirectory + "\\plugins\\");
+
+            //
+            IInterfacesManager interfacesMng = new InterfacesManager(loggerMng, pluginMng);
+            IVrManager vrMng = new VrManager(loggerMng, pluginMng);
+
+            //Create main instance
             ohm = new OpenHomeMation(pluginMng, dataMng, loggerMng, interfacesMng, vrMng);
 
-            this.EventLog.WriteEntry("OHM: Starting");
+            //Write event starting
+            this.EventLog.WriteEntry("OHM.Service: OnStart: OHM Starting", System.Diagnostics.EventLogEntryType.Information);
+
             bool result = ohm.Start();
+
             if (result)
             {
-                this.EventLog.WriteEntry("OHM: Started");
+                this.EventLog.WriteEntry("OHM.Service: OnStart: OHM Started", System.Diagnostics.EventLogEntryType.SuccessAudit);
             }
             else
             {
-                this.EventLog.WriteEntry("OHM: Not started");
+                this.EventLog.WriteEntry("OHM.Service: OnStart: OHM Not started", System.Diagnostics.EventLogEntryType.Error, 1);
             }
+
+            base.OnStart(args);
         }
 
-        protected override bool OnPowerEvent(PowerBroadcastStatus powerStatus)
-        {
-            switch (powerStatus)
-            {
-                case PowerBroadcastStatus.BatteryLow:
-
-                    break;
-                case PowerBroadcastStatus.OemEvent:
-
-                    break;
-                case PowerBroadcastStatus.PowerStatusChange:
-
-                    break;
-                case PowerBroadcastStatus.QuerySuspend:
-
-                    break;
-                case PowerBroadcastStatus.QuerySuspendFailed:
-
-                    break;
-                case PowerBroadcastStatus.ResumeAutomatic:
-
-                    break;
-                case PowerBroadcastStatus.ResumeCritical:
-
-                    break;
-                case PowerBroadcastStatus.ResumeSuspend:
-
-                    break;
-                case PowerBroadcastStatus.Suspend:
-
-                    break;
-            }
-            return base.OnPowerEvent(powerStatus);
-        }
-
+        /// <summary>
+        /// Shutdown the system
+        /// </summary>
         protected override void OnStop()
         {
+            //TODO: Really shutdown? 
+            //We should make a difference between stop and shutdown
             ohm.Shutdown();
+            base.OnStop();
         }
 
+        /// <summary>
+        /// Shutdown the system (Same as Stop)
+        /// </summary>
+        protected override void OnShutdown()
+        {
+            ohm.Shutdown();
+            base.OnShutdown();
+        }
+
+        /// <summary>
+        /// Handle custom command
+        /// </summary>
+        /// <param name="command">Integer of the requested command</param>
         protected override void OnCustomCommand(int command)
         {
             base.OnCustomCommand(command);
-            //between 128 CanPauseAndContinue 256 inclusive
+            //between 128 - 256 inclusive
             switch (command)
             {
                 case 128:
@@ -105,27 +150,81 @@ namespace OpenHomeMationService
             }
         }
 
+        /// <summary>
+        /// Handle power event
+        /// </summary>
+        /// <remarks>
+        /// TODO: notify system with new power status.
+        /// </remarks>
+        protected override bool OnPowerEvent(PowerBroadcastStatus powerStatus)
+        {
+            switch (powerStatus)
+            {
+                case PowerBroadcastStatus.BatteryLow:
+                    //
+                    break;
+                case PowerBroadcastStatus.OemEvent:
+                    //
+                    break;
+                case PowerBroadcastStatus.PowerStatusChange:
+                    //
+                    break;
+                case PowerBroadcastStatus.QuerySuspend:
+                    //
+                    break;
+                case PowerBroadcastStatus.QuerySuspendFailed:
+                    //
+                    break;
+                case PowerBroadcastStatus.ResumeAutomatic:
+                    //
+                    break;
+                case PowerBroadcastStatus.ResumeCritical:
+                    //
+                    break;
+                case PowerBroadcastStatus.ResumeSuspend:
+                    //
+                    break;
+                case PowerBroadcastStatus.Suspend:
+                    //
+                    break;
+            }
+
+            return base.OnPowerEvent(powerStatus);
+        }
+
+        #region Pause And Continue
+
+        /// <summary>
+        /// NOT SUPPORTED
+        /// </summary>
         protected override void OnContinue()
         {
             base.OnContinue();
         }
 
+        /// <summary>
+        /// NOT SUPPORTED
+        /// </summary>
         protected override void OnPause()
         {
             base.OnPause();
         }
 
-        protected override void OnShutdown()
+        #endregion
+
+        #endregion
+
+        #region Private method
+
+        /// <summary>
+        /// Extract data from args if available
+        /// </summary>
+        /// <param name="args"></param>
+        private void ParseArgs(string[] args)
         {
-            base.OnShutdown();
+
         }
 
-        protected override bool CanRaiseEvents
-        {
-            get
-            {
-                return base.CanRaiseEvents;
-            }
-        }
+        #endregion
     }
 }
